bf0d6e78112619b871dcf9c2e7f77c7905259233
Maman <mamankhan99@gmail.com>
2024-08-01 10:29:32 +0200

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Maman Khan on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
